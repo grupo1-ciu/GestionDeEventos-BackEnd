663f43dee938121326eae1ba33ee993e1d44fd44
adaeron <sebastian.milanesi@gmail.com>
2024-09-27 19:27:45 -0300

    add: proyecto spring

---
+++ src/main/java/ciu/grupo1/GestionDeEventosApplication.java
@@ -0,0 +1,13 @@
+package ciu.grupo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeEventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ciu/grupo1/GestionDeEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package ciu.grupo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
