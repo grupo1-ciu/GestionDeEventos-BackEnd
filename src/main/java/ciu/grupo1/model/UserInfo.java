8eafdc24174592b1ee66b01119e669948b4a2ed8
Cramer666 <luciaperrone.unq@gmail.com>
2024-10-04 18:53:31 -0300

    index

--- src/main/java/ciu/grupo1/model/UserInfo.java
+++ src/main/java/ciu/grupo1/model/UserInfo.java
@@ -4,53 +4,67 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="UserInfo", schema = "eventos")
+@Table(name = "UserInfo", schema = "eventos", indexes = {
+		@Index(name = "index_UserInfo_email", columnList = "email", unique = true) })
 public class UserInfo {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String name;
-    private String lastName;
-    private String email;
-    private String password;
-    private String roles;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String lastName;
+	private String email;
+	private String password;
+	private String roles;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getRoles() {
 		return roles;
 	}
+
 	public void setRoles(String roles) {
 		this.roles = roles;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
