e397c7767c7718a4de15414b2c9021b92cf76718
Cramer666 <luciaperrone.unlp@gmail.com>
2024-11-20 19:26:41 -0300

    Hice un InscripcionRequestDto donde set el emailUsuario y el idEvento para pasarlo como parametro en inscribirUsuario del InscripcionController

--- src/main/java/ciu/grupo1/controller/InscripcionController.java
+++ src/main/java/ciu/grupo1/controller/InscripcionController.java
@@ -6,10 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ciu.grupo1.dto.InscripcionDto;
+import ciu.grupo1.dto.InscripcionRequestDto;
 import ciu.grupo1.model.Evento;
 import ciu.grupo1.model.Inscripcion;
 import ciu.grupo1.model.Usuario;
@@ -36,49 +36,41 @@ public class InscripcionController {
 
     @PostMapping
     @PreAuthorize("hasAuthority('ROLE_USER')")
-    public ResponseEntity<String> inscribirUsuario(@RequestParam UUID idEvento, @RequestParam UUID idUsuario) {
+    public ResponseEntity<String> inscribirUsuario(@RequestBody InscripcionRequestDto inscripcionRequest) {
 
-        Usuario usuario = usuarioService.obtenerUsuarioPorId(idUsuario);
+        UUID idEvento = inscripcionRequest.getIdEvento();
+        String emailUsuario = inscripcionRequest.getEmailUsuario();
+
+        Usuario usuario = usuarioService.getByEmail(emailUsuario).orElse(null);
         if (usuario == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario no encontrado");
         }
 
- 
         if (inscripcionService.estaInscrito(usuario.getId(), idEvento)) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("El usuario ya está inscrito en el evento");
         }
-
 
         Evento evento = eventoService.obtenerEventoPorId(idEvento);
         if (evento == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Evento no encontrado");
         }
 
- 
-        int cuposDisponibles = (int) (evento.getCapacidad()- inscripcionService.contarInscripciones(idEvento));
+        int cuposDisponibles = (int) (evento.getCapacidad() - inscripcionService.contarInscripciones(idEvento));
         if (cuposDisponibles <= 0) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No hay cupos disponibles para el evento");
         }
 
-       
         if (!evento.getEstado().getNombreEstadoEvento().name().equalsIgnoreCase("DISPONIBLE")) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("El evento no está abierto para inscripciones");
         }
-        System.err.println("idEvento: "+idEvento);
-        System.err.println("idUsuario: "+idUsuario);
 
         Inscripcion inscripcion = new Inscripcion();
         inscripcion.setUsuario(usuario);
         inscripcion.setEvento(evento);
-        inscripcion.setEstadoInscripcion(this.estadoInscripcionService.setEstadoPendiente()); // Ajusta según sea necesario
-        System.err.println("idInscripcion: "+inscripcion.getId());
+        inscripcion.setEstadoInscripcion(this.estadoInscripcionService.setEstadoPendiente());
         inscripcionService.guardarInscripcion(inscripcion);
-
-
-        InscripcionDto inscripcionDto = new InscripcionDto();
-        inscripcionDto.setId(inscripcion.getId().toString());
-       
 
         return ResponseEntity.ok("Inscripción realizada con éxito");
     }
+
 }
